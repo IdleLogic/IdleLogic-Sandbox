aba53d8f53dbb5dcf4e7ed29f6f0af7b6dcbdf16
Chris Zeh <Chris.W.Zeh@gmail.com>
2011-08-08 20:55:59 -0600

    First Pythong File Addition

---
+++ HelloWorld.py
@@ -0,0 +1,16 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Chris
+#
+# Created:     08/08/2011
+# Copyright:   (c) Chris 2011
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    print "Hello GitHub"
+
+if __name__ == '__main__':
+    main()
